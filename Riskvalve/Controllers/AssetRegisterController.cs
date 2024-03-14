8746eb2527cac5b488d440ebf24bce07e54a0a34
Aldo <aldo.febrian.id@gmail.com>
2024-03-15 06:59:17 +0700

    CRUD ASSET

--- Riskvalve/Controllers/AssetRegisterController.cs
+++ Riskvalve/Controllers/AssetRegisterController.cs
@@ -33,11 +33,12 @@ public class AssetRegisterController : Controller
     [HttpPost]
     public IActionResult UpdateArea()
     {
-        AreaModel areaModel = new()
-        {
-            Id = Convert.ToInt32(Request.Form["Id"]),
-            BusinessArea = Request.Form["BusinessArea"]
-        };
+        AreaModel areaModel =
+            new()
+            {
+                Id = Convert.ToInt32(Request.Form["Id"]),
+                BusinessArea = Request.Form["BusinessArea"]
+            };
         areaModel.UpdateArea(areaModel);
         return RedirectToAction("Area");
     }
@@ -45,10 +46,7 @@ public class AssetRegisterController : Controller
     [HttpPost]
     public IActionResult AddArea()
     {
-        AreaModel areaModel = new()
-        {
-            BusinessArea = Request.Form["BusinessArea"]
-        };
+        AreaModel areaModel = new() { BusinessArea = Request.Form["BusinessArea"] };
         areaModel.AddArea(areaModel);
         return RedirectToAction("Area");
     }
@@ -85,13 +83,14 @@ public class AssetRegisterController : Controller
     public IActionResult UpdatePlatform()
     {
         PlatformModel platformModel = new();
-        PlatformDB platformDb = new()
-        {
-            Id = Convert.ToInt32(Request.Form["Id"]),
-            AreaID = Convert.ToInt32(Request.Form["AreaID"]),
-            Platform = Request.Form["Platform"],
-            Code = Request.Form["Code"]
-        };
+        PlatformDB platformDb =
+            new()
+            {
+                Id = Convert.ToInt32(Request.Form["Id"]),
+                AreaID = Convert.ToInt32(Request.Form["AreaID"]),
+                Platform = Request.Form["Platform"],
+                Code = Request.Form["Code"]
+            };
         platformModel.UpdatePlatform(platformDb);
         return RedirectToAction("Platform");
     }
@@ -100,12 +99,13 @@ public class AssetRegisterController : Controller
     public IActionResult AddPlatform()
     {
         PlatformModel platformModel = new();
-        PlatformDB platformDb = new()
-        {
-            AreaID = Convert.ToInt32(Request.Form["AreaID"]),
-            Platform = Request.Form["Platform"],
-            Code = Request.Form["Code"]
-        };
+        PlatformDB platformDb =
+            new()
+            {
+                AreaID = Convert.ToInt32(Request.Form["AreaID"]),
+                Platform = Request.Form["Platform"],
+                Code = Request.Form["Code"]
+            };
         platformModel.AddPlatform(platformDb);
         return RedirectToAction("Platform");
     }
@@ -121,13 +121,135 @@ public class AssetRegisterController : Controller
 
     public IActionResult Asset()
     {
+        AssetModel assetModel = new();
+        List<AssetModel> assetList = assetModel.GetAssetList();
+        ViewData["AssetList"] = assetList;
+
+        List<PlatformModel> platformList = new PlatformModel().GetPlatformList();
+        ViewData["PlatformList"] = platformList;
+
+        List<ValveTypeModel> valveTypeList = new ValveTypeModel().GetValveTypeList();
+        ViewData["ValveTypeList"] = valveTypeList;
+
+        List<ManualOverrideModel> manualOverrideList =
+            new ManualOverrideModel().GetManualOverrideList();
+        ViewData["ManualOverrideList"] = manualOverrideList;
+
+        List<FluidPhaseModel> fluidPhaseList = new FluidPhaseModel().GetFluidPhaseList();
+        ViewData["FluidPhaseList"] = fluidPhaseList;
+
+        List<ToxicOrFlamableFluidModel> toxicOrFlamableFluidList =
+            new ToxicOrFlamableFluidModel().GetToxicOrFlamableFluidList();
+        ViewData["ToxicOrFlamableFluidList"] = toxicOrFlamableFluidList;
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult GetAssetList()
+    {
+        AssetModel assetModel = new();
+        List<AssetModel> assetList = assetModel.GetAssetList();
+        return Json(assetList);
+    }
+
+    [HttpGet]
+    public IActionResult GetAssetDetail()
+    {
+        AssetModel assetModel = new();
+        int id = Convert.ToInt32(Request.Query["id"]);
+        AssetModel asset = assetModel.GetAssetModel(id);
+        return Json(asset);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateAsset()
+    {
+        AssetModel assetModel = new();
+        AssetDB assetDb =
+            new()
+            {
+                Id = Convert.ToInt32(Request.Form["Id"]),
+                TagNo = Request.Form["TagNo"],
+                PlatformID = Convert.ToInt32(Request.Form["PlatformID"]),
+                ValveTypeID = Convert.ToInt32(Request.Form["ValveTypeID"]),
+                Size = Request.Form["Size"],
+                ClassRating = Request.Form["ClassRating"],
+                ParentEquipmentNo = Request.Form["ParentEquipmentNo"],
+                ParentEquipmentDescription = Request.Form["ParentEquipmentDescription"],
+                InstallationDate = Request.Form["InstallationDate"],
+                PIDNo = Request.Form["PIDNo"],
+                Manufacturer = Request.Form["Manufacturer"],
+                BodyModel = Request.Form["BodyModel"],
+                BodyMaterial = Request.Form["BodyMaterial"],
+                EndConnection = Request.Form["EndConnection"],
+                SerialNo = Request.Form["SerialNo"],
+                ManualOverrideID = Convert.ToInt32(Request.Form["ManualOverrideID"]),
+                ActuatorMfg = Request.Form["ActuatorMfg"],
+                ActuatorSerialNo = Request.Form["ActuatorSerialNo"],
+                ActuatorTypeModel = Request.Form["ActuatorTypeModel"],
+                ActuatorPower = Request.Form["ActuatorPower"],
+                OperatingTemperature = Request.Form["OperatingTemperature"],
+                OperatingPressure = Request.Form["OperatingPressure"],
+                FlowRate = Request.Form["FlowRate"],
+                ServiceFluid = Request.Form["ServiceFluid"],
+                FluidPhaseID = Convert.ToInt32(Request.Form["FluidPhaseID"]),
+                ToxicOrFlamableFluidID = Convert.ToInt32(Request.Form["ToxicOrFlamableFluidID"])
+            };
+        assetModel.UpdateAsset(assetDb);
+        return RedirectToAction("Asset");
+    }
+
+    [HttpPost]
+    public IActionResult AddAsset()
+    {
+        AssetModel assetModel = new();
+        AssetDB assetDb =
+            new()
+            {
+                TagNo = Request.Form["TagNo"],
+                PlatformID = Convert.ToInt32(Request.Form["PlatformID"]),
+                ValveTypeID = Convert.ToInt32(Request.Form["ValveTypeID"]),
+                Size = Request.Form["Size"],
+                ClassRating = Request.Form["ClassRating"],
+                ParentEquipmentNo = Request.Form["ParentEquipmentNo"],
+                ParentEquipmentDescription = Request.Form["ParentEquipmentDescription"],
+                InstallationDate = Request.Form["InstallationDate"],
+                PIDNo = Request.Form["PIDNo"],
+                Manufacturer = Request.Form["Manufacturer"],
+                BodyModel = Request.Form["BodyModel"],
+                BodyMaterial = Request.Form["BodyMaterial"],
+                EndConnection = Request.Form["EndConnection"],
+                SerialNo = Request.Form["SerialNo"],
+                ManualOverrideID = Convert.ToInt32(Request.Form["ManualOverrideID"]),
+                ActuatorMfg = Request.Form["ActuatorMfg"],
+                ActuatorSerialNo = Request.Form["ActuatorSerialNo"],
+                ActuatorTypeModel = Request.Form["ActuatorTypeModel"],
+                ActuatorPower = Request.Form["ActuatorPower"],
+                OperatingTemperature = Request.Form["OperatingTemperature"],
+                OperatingPressure = Request.Form["OperatingPressure"],
+                FlowRate = Request.Form["FlowRate"],
+                ServiceFluid = Request.Form["ServiceFluid"],
+                FluidPhaseID = Convert.ToInt32(Request.Form["FluidPhaseID"]),
+                ToxicOrFlamableFluidID = Convert.ToInt32(Request.Form["ToxicOrFlamableFluidID"])
+            };
+        assetModel.AddAsset(assetDb);
+        return RedirectToAction("Asset");
+    }
+
+    [HttpPost]
+    public IActionResult DeleteAsset()
+    {
+        AssetModel assetModel = new();
+        int id = Convert.ToInt32(Request.Form["Id"]);
+        assetModel.DeleteAsset(id);
+        return RedirectToAction("Asset");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
 }
-
--- Riskvalve/Models/AssetModel.cs
+++ Riskvalve/Models/AssetModel.cs
@@ -1,26 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Riskvalve.Models;
 
-public class AssetModel
+public class AssetContext : DbContext
+{
+    public DbSet<AssetDB> Asset { get; set; }
+    public DbSet<PlatformDB> Platform { get; set; }
+    public DbSet<AreaModel> Area { get; set; }
+    public DbSet<ValveTypeModel> ValveType { get; set; }
+    public DbSet<ManualOverrideModel> ManualOverride { get; set; }
+    public DbSet<FluidPhaseModel> FluidPhase { get; set; }
+    public DbSet<ToxicOrFlamableFluidModel> ToxicOrFlamableFluid { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options
+            .UseSqlServer(
+                "Server=127.0.0.1,1433;Database=Riskvalve;User Id=SA;Password=DB_Password;Encrypt=False;Connection Timeout=30;"
+            )
+            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+}
+
+public class AssetDB
 {
     public int Id { get; set; }
     public string? TagNo { get; set; }
-    // public PlatformModel? Platform { get; set; }
-    public string? PlatformID { get; set; }
-    // public ValveTypeModel? ValveType { get; set; }
-    public int ValveTypeID { get; set; }
-    public int Size { get; set; }
-    public int ClassRating { get; set; }
-    public string? ParentEquitpmentNo { get; set; }
+    public int? PlatformID { get; set; } // FK
+    public int ValveTypeID { get; set; } // FK
+    public string? Size { get; set; }
+    public string? ClassRating { get; set; }
+    public string? ParentEquipmentNo { get; set; }
     public string? ParentEquipmentDescription { get; set; }
-    public DateTime InstallationDate { get; set; }
+    public string? InstallationDate { get; set; }
     public string? PIDNo { get; set; }
     public string? Manufacturer { get; set; }
     public string? BodyModel { get; set; }
     public string? BodyMaterial { get; set; }
     public string? EndConnection { get; set; }
     public string? SerialNo { get; set; }
-    // public ManualOverrideModel? ManualOverride { get; set; }
-    public int ManualOverrideID { get; set; }
+    public int ManualOverrideID { get; set; } // FK
     public string? ActuatorMfg { get; set; }
     public string? ActuatorSerialNo { get; set; }
     public string? ActuatorTypeModel { get; set; }
@@ -29,10 +46,154 @@ public class AssetModel
     public string? OperatingPressure { get; set; }
     public string? FlowRate { get; set; }
     public string? ServiceFluid { get; set; }
-    // public FluidPhaseModel? FluidPhase { get; set; }
-    public int FluidPhaseID { get; set; }
-    // public ToxicOrFlamableFluidModel? ToxicOrFlamableFluid { get; set; }
-    public int ToxicOrFlamableFluidID { get; set; }
+    public int FluidPhaseID { get; set; } // FK
+    public int ToxicOrFlamableFluidID { get; set; } // FK
+}
+
+public class AssetModel : AssetDB
+{
+    public string? Platform { get; set; }
+    public string? BusinessArea { get; set; }
+    public string? ValveType { get; set; }
+    public string? ManualOverride { get; set; }
+    public string? FluidPhase { get; set; }
+    public string? ToxicOrFlamableFluid { get; set; }
+
+    public AssetModel GetAssetModel(int id)
+    {
+        AssetModel asset = new();
+        using (var context = new AssetContext())
+        {
+            List<AssetModel> assetList = (
+                from a in context.Asset
+                join p in context.Platform on a.PlatformID equals p.Id
+                join ar in context.Area on p.AreaID equals ar.Id
+                join v in context.ValveType on a.ValveTypeID equals v.Id
+                join m in context.ManualOverride on a.ManualOverrideID equals m.Id
+                join f in context.FluidPhase on a.FluidPhaseID equals f.Id
+                join t in context.ToxicOrFlamableFluid on a.ToxicOrFlamableFluidID equals t.Id
+                where a.Id == id
+                select new AssetModel
+                {
+                    Id = a.Id,
+                    TagNo = a.TagNo,
+                    PlatformID = a.PlatformID,
+                    ValveTypeID = a.ValveTypeID,
+                    Size = a.Size,
+                    ClassRating = a.ClassRating,
+                    ParentEquipmentNo = a.ParentEquipmentNo,
+                    ParentEquipmentDescription = a.ParentEquipmentDescription,
+                    InstallationDate = a.InstallationDate,
+                    PIDNo = a.PIDNo,
+                    Manufacturer = a.Manufacturer,
+                    BodyModel = a.BodyModel,
+                    BodyMaterial = a.BodyMaterial,
+                    EndConnection = a.EndConnection,
+                    SerialNo = a.SerialNo,
+                    ManualOverrideID = a.ManualOverrideID,
+                    ActuatorMfg = a.ActuatorMfg,
+                    ActuatorSerialNo = a.ActuatorSerialNo,
+                    ActuatorTypeModel = a.ActuatorTypeModel,
+                    ActuatorPower = a.ActuatorPower,
+                    OperatingTemperature = a.OperatingTemperature,
+                    OperatingPressure = a.OperatingPressure,
+                    FlowRate = a.FlowRate,
+                    ServiceFluid = a.ServiceFluid,
+                    FluidPhaseID = a.FluidPhaseID,
+                    ToxicOrFlamableFluidID = a.ToxicOrFlamableFluidID,
+                    Platform = p!.Platform,
+                    BusinessArea = ar!.BusinessArea,
+                    ValveType = v!.ValveType,
+                    ManualOverride = m!.ManualOverride,
+                    FluidPhase = f!.FluidPhase,
+                    ToxicOrFlamableFluid = t!.ToxicOrFlamableFluid
+                }
+            ).ToList();
+            asset = assetList[0];
+        }
+        return asset;
+    }
+
+    public List<AssetModel> GetAssetList()
+    {
+        List<AssetModel> assetList = new();
+        using (var context = new AssetContext())
+        {
+            assetList = (
+                from a in context.Asset
+                join p in context.Platform on a.PlatformID equals p.Id
+                join ar in context.Area on p.AreaID equals ar.Id
+                join v in context.ValveType on a.ValveTypeID equals v.Id
+                join m in context.ManualOverride on a.ManualOverrideID equals m.Id
+                join f in context.FluidPhase on a.FluidPhaseID equals f.Id
+                join t in context.ToxicOrFlamableFluid on a.ToxicOrFlamableFluidID equals t.Id
+                select new AssetModel
+                {
+                    Id = a.Id,
+                    TagNo = a.TagNo,
+                    PlatformID = a.PlatformID,
+                    ValveTypeID = a.ValveTypeID,
+                    Size = a.Size,
+                    ClassRating = a.ClassRating,
+                    ParentEquipmentNo = a.ParentEquipmentNo,
+                    ParentEquipmentDescription = a.ParentEquipmentDescription,
+                    InstallationDate = a.InstallationDate,
+                    PIDNo = a.PIDNo,
+                    Manufacturer = a.Manufacturer,
+                    BodyModel = a.BodyModel,
+                    BodyMaterial = a.BodyMaterial,
+                    EndConnection = a.EndConnection,
+                    SerialNo = a.SerialNo,
+                    ManualOverrideID = a.ManualOverrideID,
+                    ActuatorMfg = a.ActuatorMfg,
+                    ActuatorSerialNo = a.ActuatorSerialNo,
+                    ActuatorTypeModel = a.ActuatorTypeModel,
+                    ActuatorPower = a.ActuatorPower,
+                    OperatingTemperature = a.OperatingTemperature,
+                    OperatingPressure = a.OperatingPressure,
+                    FlowRate = a.FlowRate,
+                    ServiceFluid = a.ServiceFluid,
+                    FluidPhaseID = a.FluidPhaseID,
+                    ToxicOrFlamableFluidID = a.ToxicOrFlamableFluidID,
+                    Platform = p!.Platform,
+                    BusinessArea = ar!.BusinessArea,
+                    ValveType = v!.ValveType,
+                    ManualOverride = m!.ManualOverride,
+                    FluidPhase = f!.FluidPhase,
+                    ToxicOrFlamableFluid = t!.ToxicOrFlamableFluid
+                }
+            ).ToList();
+        }
+        return assetList;
+    }
+
+    public void AddAsset(AssetDB asset)
+    {
+        using (var context = new AssetContext())
+        {
+            context.Asset.Add(asset);
+            context.SaveChanges();
+        }
+    }
+
+    public void UpdateAsset(AssetDB asset)
+    {
+        using (var context = new AssetContext())
+        {
+            context.Asset.Update(asset);
+            context.SaveChanges();
+        }
+    }
+
+    public void DeleteAsset(int id)
+    {
+        using (var context = new AssetContext())
+        {
+            AssetDB asset = context.Asset.Find(id);
+            context.Asset.Remove(asset);
+            context.SaveChanges();
+        }
+    }
 }
 
 public class ValveTypeModel
@@ -40,16 +201,13 @@ public class ValveTypeModel
     public int Id { get; set; }
     public string? ValveType { get; set; }
 
-    public static List<ValveTypeModel> GetValveTypeList()
+    public List<ValveTypeModel> GetValveTypeList()
     {
-        List<ValveTypeModel> valveTypeList =
-            new()
-            {
-                new() { Id = 1, ValveType = "Air Release Valve" },
-                new() { Id = 2, ValveType = "Blowdown Valve" },
-                new() { Id = 3, ValveType = "Check Valve" },
-                new() { Id = 4, ValveType = "Deluge Valve" },
-            };
+        List<ValveTypeModel> valveTypeList = new();
+        using (var context = new AssetContext())
+        {
+            valveTypeList = context.ValveType.ToList();
+        }
         return valveTypeList;
     }
 }
@@ -57,17 +215,15 @@ public class ValveTypeModel
 public class ManualOverrideModel
 {
     public int Id { get; set; }
-    public string? Text { get; set; }
+    public string? ManualOverride { get; set; }
 
-    public static List<ManualOverrideModel> GetManualOverrideList()
+    public List<ManualOverrideModel> GetManualOverrideList()
     {
-        List<ManualOverrideModel> manualOverrideList =
-            new()
-            {
-                new() { Id = 1, Text = "Yes" },
-                new() { Id = 2, Text = "No" },
-                new() { Id = 2, Text = "Normally Open or Close" }
-            };
+        List<ManualOverrideModel> manualOverrideList = new();
+        using (var context = new AssetContext())
+        {
+            manualOverrideList = context.ManualOverride.ToList();
+        }
         return manualOverrideList;
     }
 }
@@ -77,14 +233,13 @@ public class FluidPhaseModel
     public int Id { get; set; }
     public string? FluidPhase { get; set; }
 
-    public static List<FluidPhaseModel> GetFluidPhaseList()
+    public List<FluidPhaseModel> GetFluidPhaseList()
     {
-        List<FluidPhaseModel> fluidPhaseList =
-            new()
-            {
-                new() { Id = 1, FluidPhase = "Liquid" },
-                new() { Id = 2, FluidPhase = "Gas" }
-            };
+        List<FluidPhaseModel> fluidPhaseList = new();
+        using (var context = new AssetContext())
+        {
+            fluidPhaseList = context.FluidPhase.ToList();
+        }
         return fluidPhaseList;
     }
 }
@@ -94,14 +249,13 @@ public class ToxicOrFlamableFluidModel
     public int Id { get; set; }
     public string? ToxicOrFlamableFluid { get; set; }
 
-    public static List<ToxicOrFlamableFluidModel> GetToxicOrFlamableFluidList()
+    public List<ToxicOrFlamableFluidModel> GetToxicOrFlamableFluidList()
     {
-        List<ToxicOrFlamableFluidModel> toxicOrFlamableFluidList =
-            new()
-            {
-                new() { Id = 1, ToxicOrFlamableFluid = "Yes" },
-                new() { Id = 2, ToxicOrFlamableFluid = "No" }
-            };
+        List<ToxicOrFlamableFluidModel> toxicOrFlamableFluidList = new();
+        using (var context = new AssetContext())
+        {
+            toxicOrFlamableFluidList = context.ToxicOrFlamableFluid.ToList();
+        }
         return toxicOrFlamableFluidList;
     }
 }
