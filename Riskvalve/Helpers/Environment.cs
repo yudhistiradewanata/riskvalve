d6b201c14cdc00ddef77f1931e26524c46270cff
Aldo <aldo.febrian.id@gmail.com>
2024-04-02 09:36:06 +0700

    up https

--- Riskvalve/Helpers/Environment.cs
+++ Riskvalve/Helpers/Environment.cs
@@ -5,7 +5,11 @@ public static class Environment
 
     public static string GetConnectionStringDB()
     {
-        return _DbAldo;
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+        return configuration?.GetConnectionString("DefaultConnection") ?? "";
     }
 
     public static string GetDateFormatString()
